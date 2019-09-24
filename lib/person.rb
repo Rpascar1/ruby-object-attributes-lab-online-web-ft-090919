221042ea2cf670653fb2fb8f8e1882f8c0fa11d1
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-24 01:26:32 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,20 +1,20 @@
 def Person
 
- def name=(name)
+  def name=(name)
    @name = name
- end
+  end
  
- def name
- @name
- end
+  def name
+    @name
+  end
 
- def job=(job) 
- @job = job
- end
+  def job=(job) 
+    @job = job
+  end
 
- def job
- @job
- end
+  def job
+    @job
+  end
 
 
 end
